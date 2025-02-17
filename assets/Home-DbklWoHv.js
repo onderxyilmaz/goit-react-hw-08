bfcf65b67276db0b90469b3b3bf797701094edfa
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-02-17 23:42:29 +0300

    Updates

--- assets/Home-DbklWoHv.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DQU8KQuV.js";const t={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function n(){return e.jsx("div",{style:t.container,children:e.jsx("h1",{style:t.title,children:"Kişi Rehberi Uygulamasına Hoş Geldiniz"})})}export{n as default};
