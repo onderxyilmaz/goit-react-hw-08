efd09fa749a17ed6ab262baaae6c86404cec4b16
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-02-17 23:43:43 +0300

    Updated build configuration

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production'
     ? '/goit-react-hw-08/'
-    : '/'
+    : '/',
+  build: {
+    outDir: 'dist',
+  }
 })
